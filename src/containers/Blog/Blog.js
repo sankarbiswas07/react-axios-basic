850696b1a5993a16b5137a97161569fe76c3efa5
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2020-07-12 00:19:09 +0530

    Link component from react-router-dom with some extra example options in new-post link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import axios from "axios";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
 import "./Blog.css";
@@ -13,10 +13,14 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a href="/new-post">New post</a>
+                <Link to={{
+                  pathname:"/new-post",
+                  hash:"#submit",
+                  search:"?quick-submit=true"
+                }}>New Post</Link>
               </li>
             </ul>
           </nav>
